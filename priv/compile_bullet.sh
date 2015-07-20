7d46df62d97d276a75c039bb1f03791f737f9737
Yan Guiborat <yan.guiborat@gmail.com>
2015-07-20 17:16:33 +0200

    Compilation working when project included into another one

--- priv/compile_bullet.sh
+++ priv/compile_bullet.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 echo Compiling Bullet from `pwd`
-cd ./deps/bullet/build3
+
+if [ -f ./deps/bullet3 ]
+then
+    echo Found previous bullet installation, skipping download.
+
+    else
+    echo Fetching Bullet to `pwd`
+    mkdir -p deps
+    git clone https://github.com/bulletphysics/bullet3.git
+    mv bullet3 deps/
+fi
+
+
+cd ./deps/bullet3/build3
 ./premake4_osx gmake
 cd gmake
 make
